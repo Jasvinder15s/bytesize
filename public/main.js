fef6732532b1b9e5fcf4ba9f299eacde8f0c20ff
Jasvinder Singh <jasvinder15atech@gmail.com>
2020-09-22 00:56:20 +0530

    added boilerplate code for async await post request

--- public/main.js
+++ public/main.js
@@ -1,37 +1,15 @@
-// Information to reach API
-const apiKey = "7f20e441461f4942a01ea76c1c3517ea";
-const url = "https://api.rebrandly.com/v1/links";
-
-// Some page elements
-const inputField = document.querySelector("#input");
-const shortenButton = document.querySelector("#shorten");
-const responseField = document.querySelector("#responseField");
-
-// AJAX functions
-const shortenUrl = () => {
-  const urlToShorten = inputField.value;
-  const data = JSON.stringify({ destination: urlToShorten });
-
-  const xhr = new XMLHttpRequest();
-  xhr.responseType = "json";
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      renderResponse(xhr.response);
+const getData = async () => {
+  try {
+    const response = await fetch("https://api-to-call.com/endpoint", {
+      method: "POST",
+      body: JSON.stringify({ id: 200 }),
+    });
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse();
     }
-  };
-  xhr.open("POST", url);
-  xhr.setRequestHeader("Content-type", "application/json");
-  xhr.setRequestHeader("apikey", apiKey);
-  xhr.send(data);
-};
-
-// Clear page and call AJAX functions
-const displayShortUrl = (event) => {
-  event.preventDefault();
-  while (responseField.firstChild) {
-    responseField.removeChild(responseField.firstChild);
+    throw new Error("Request failed!");
+  } catch (error) {
+    console.log(error);
   }
-  shortenUrl();
 };
-
-shortenButton.addEventListener("click", displayShortUrl);
